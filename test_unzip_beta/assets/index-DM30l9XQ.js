a0660fa8c3da7dce4f70ad0dc13055dd5faacfe5
lukcorp83-glitch <pixelozapolska@gmail.com>
2026-06-30 08:34:47 +0200

    go

---
+++ test_unzip_beta/assets/index-DM30l9XQ.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-DgNu6HTE.js","./index-ByA6Bahn.js","./index-DDjJJfjf.css"])))=>i.map(i=>d[i]);
+import{aE as e,aF as o}from"./index-ByA6Bahn.js";const i=e("Browser",{web:()=>o(()=>import("./web-DgNu6HTE.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(r=>new r.BrowserWeb)});export{i as Browser};
