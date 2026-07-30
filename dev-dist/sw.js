8b1500d4a3616097e5b1ef5541713f302b177327
lukcorp83-glitch <pixelozapolska@gmail.com>
2026-07-30 09:56:48 +0200

    fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-afac4cd2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cgrtvucfs9k"
+    "revision": "0.901th919oe8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
