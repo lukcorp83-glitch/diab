a817575870054b37beae96c53bdc3b355d8ba47d
lukcorp83-glitch <pixelozapolska@gmail.com>
2026-08-03 07:52:57 +0200

    5.9.97

--- src/constants/versions.ts
+++ src/constants/versions.ts
@@ -1,4 +1,4 @@
-import i18n from "../i18n";
+﻿import i18n from "../i18n";
 
 export interface ChangeEntry {
   categoryKey: string;
@@ -14,7 +14,7 @@ export interface VersionEntry {
   changes: (string | ChangeEntry)[];
 }
 
-export const CURRENT_VERSION = '5.9.96';
+export const CURRENT_VERSION = '5.9.97';
 
 import versionData from '../../version.json';
 export const CURRENT_OTA_REVISION = versionData.otaRevision || 0;
@@ -1144,6 +1144,7 @@ export const APK_VERSIONS: VersionEntry[] = [
     ]
   }
 ];
+
 
 
 
--- src/hooks/queries/useProfileData.ts
+++ src/hooks/queries/useProfileData.ts
@@ -98,3 +98,4 @@ export const usePumpStatus = (user: any) => {
   });
 };
 
+
