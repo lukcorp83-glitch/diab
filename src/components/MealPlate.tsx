38194badd7ceb5556bfa4daad4957ee1d6e1a52f
lukcorp83-glitch <pixelozapolska@gmail.com>
2026-08-31 08:50:39 +0200

    Update MealPlate.tsx

--- src/components/MealPlate.tsx
+++ src/components/MealPlate.tsx
@@ -1,3 +1,4 @@
+import { useAppStore } from '../stores/useAppStore';
 import { useAuthStore } from '../stores/useAuthStore';
 import { useMealPlateLogic } from '../hooks/useMealPlateLogic';
 import { ProductSearch } from "./MealPlate/ProductSearch";
